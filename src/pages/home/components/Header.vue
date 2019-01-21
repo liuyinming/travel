93ca5e4d8211c4992b00ed36d04acee83cc152d9
liuyinming <1126804398@qq.com>
2019-01-21 14:35:57 +0800

    'project'

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -5,10 +5,15 @@
        </div> 
        <div class="header-input">
            <span class="iconfont arrow-icon" >&#xe74b;</span>
-           输入城市/景点/游玩主题</div> 
-       <div class="header-right">{{this.city}}
-           <span class="iconfont">&#xe64a;</span></div> 
-    </div>
+           输入城市/景点/游玩主题
+        </div> 
+        <router-link to="/city">  
+            <div class="header-right">{{this.city}}
+                  <span class="iconfont">&#xe64a;</span>  
+            </div> 
+        </router-link>
+        </div>
+      
 </template>
 
 <script>
@@ -47,6 +52,7 @@ export default {
       width: 1.24rem
       float: right
       text-align: center
+      color :#fff
       .arrow-icon
         font-size :.24rem
 </style>
