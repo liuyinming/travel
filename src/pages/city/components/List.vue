40e168bb18939e786f83e87f17d242a904155481
liuyinming <1126804398@qq.com>
2019-01-24 10:48:18 +0800

    'project'

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -5,19 +5,20 @@
         <div class="title border-topbottom">当前城市</div>
         <div class="button-list">
           <div class="button-wrapper">
-            <div class="button">北京</div>
+            <div class="button">{{this.currentCity}}</div>
           </div>
         </div>
       </div>
       <div class="title border-topbottom">热门城市</div>
       <div class="button-list">
-        <div class="button-wrapper" v-for="item of hotCities" :key="item.id" >
+        <div class="button-wrapper" v-for="item of hotCities" :key="item.id" 
+        @click="handleCityClick(item.name)">
           <div class="button">{{item.name}}</div>
         </div>
       </div>
-      <div class="area"  v-for="(item, key) of cities" :key="key" :ref="key">
+      <div class="area"  v-for="(item, key) of cities" :key="key" :ref="key" >
         <div class="title border-topbottom">{{key}}</div>   <!-- 这里的Key代表的是对象  -->
-        <div class="item-list" v-for="innerItem of item" :key="innerItem.id">
+        <div class="item-list" v-for="innerItem of item" :key="innerItem.id" @click="handleCityClick(innerItem.name)">
           <div class="item border-bottom">{{innerItem.name}}</div>
         </div>
       </div>
@@ -28,12 +29,26 @@
 <script>
 import BScroll from 'better-scroll'
 import { readlink } from 'fs';
+import {mapState, mapMutations} from 'vuex'
 export default {
   name: "CityList",
   props:{
       hotCities:Array,
       cities:Object,
       letter:String,
+  },
+  methods:{
+       handleCityClick(city){
+         //this.$store.commit('changeCity',city);
+         this.changeCity(city);
+         this.$router.push('/');
+       },
+       ...mapMutations(['changeCity'])
+  },
+  computed:{
+    ...mapState({
+      currentCity:'city'
+    })
   },
   mounted(){
       this.scroll = new BScroll(this.$refs.wrapper);
